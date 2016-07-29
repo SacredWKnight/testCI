e8e1a579e471ab1426bf2653be6de5f78df355bf
Yudhistira G.S <yudhis.sumardiono@gmail.com>
2016-07-29 16:11:57 +0700

    update python 3

--- aba/simplecaeser.py
+++ aba/simplecaeser.py
@@ -6,8 +6,8 @@ using a classic Caeser shift substitution (3 letter shift)"""
 import string
 
 shift = 3
-choice = raw_input("would you like to encode or decode?")
-word = (raw_input("Please enter text"))
+choice = input("would you like to encode or decode?")
+word = (input("Please enter text"))
 letters = string.ascii_letters + string.punctuation + string.digits
 encoded = ''
 if choice == "encode":
