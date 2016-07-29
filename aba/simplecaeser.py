cf89643562b74e7d1a702bffd2183b7da9ad1339
Yudhistira G.S <yudhis.sumardiono@gmail.com>
2016-07-29 16:31:49 +0700

    update

--- aba/simplecaeser.py
+++ aba/simplecaeser.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-"""This script prompts a user to enter a message to encode or decode
-using a classic Caeser shift substitution (3 letter shift)"""
-
 import string
 
 shift = 3
